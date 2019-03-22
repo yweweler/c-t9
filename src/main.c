ec1d078d0db33f070e01f26f7068fe394e513128
Yves-Noel Weweler <y.weweler@gmail.com>
2019-03-22 13:40:45 +0100

    Add a header file for main.c
    References #2

--- src/main.c
+++ src/main.c
@@ -32,12 +32,7 @@
   ******************************************************************************
   */
 
-#include <stdlib.h>
-#include <stdio.h>
-
-#include "t9/corpus.h"
-#include "t9/tree.h"
-#include "t9/timer.h"
+#include "main.h"
 
 
 void build_corpus_tree(t9_model_t *const model) {
